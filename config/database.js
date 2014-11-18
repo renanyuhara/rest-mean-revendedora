0515f892f75b0bf9d5911ae60c8fee12e089ae3c
Renan Yuhara <renan.takeshi@gmail.com>
2014-11-18 19:30:29 -0200

    First commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://renan:linx2014@proximus.modulusmongo.net:27017/Jo8basum'
+}
