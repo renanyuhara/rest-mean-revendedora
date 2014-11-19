738e05b90b287fac1865726c2da688a9a54b9f0f
Renan Yuhara <renan.takeshi@gmail.com>
2014-11-19 17:54:26 -0200

    Alterado para utilizar banco de dados local

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ module.exports = {
 
 	// the database url to connect
 	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-	url : 'mongodb://renan:linx2014@proximus.modulusmongo.net:27017/Jo8basum'
+	url : 'mongodb://localhost:27017/test'
+	//url : 'mongodb://renan:linx2014@proximus.modulusmongo.net:27017/Jo8basum'
 }
