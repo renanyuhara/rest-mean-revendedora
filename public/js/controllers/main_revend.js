53042e17cf7d2c16d8d39bba48d2214447aecfc1
Thiago Campos <thiagomcdc@hotmail.com>
2014-12-05 13:32:46 -0200

    Adicionada a primeira view de produtos

--- public/js/controllers/main_revend.js
+++ public/js/controllers/main_revend.js
@@ -64,3 +64,70 @@ angular.module('revendedoraController', [])
 				});
 		};
 	}]);
+
+angular.module('produtoController', [])
+
+	// inject the Produto service factory into our controller
+	.controller('mainController', ['$scope','$http','Produtos', function($scope, $http, Produtos) {
+		$scope.formData = {};
+		$scope.loading = true;
+
+		// GET =====================================================================
+		// when landing on the page, get all produtos and show them
+		// use the service to get all the produtos
+		Produtos.get()
+			.success(function(data) {
+				$scope.produtos = data;
+				$scope.loading = false;
+			});
+
+		// CREATE ==================================================================
+		// when submitting the add form, send the text to the node API
+		$scope.createProduto = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.nome != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				//Chama public/js/services/produtos.js
+				Produtos.criar($scope.formData)
+
+					// if successful creation, call our get function to get all the new produtos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.produtos = data; // assign our new list of produtos
+					});
+			}
+		};
+
+		$scope.updateProduto = function(id) {
+			if (id != undefined) {
+				if ($scope.formData.nome != undefined) {
+					//Chama public/js/services/produtos.js
+					Produtos.editar(id, $scope.formData)
+						.success(function(data) {
+							$scope.loading = false;
+							$scope.produtos = data;
+						});
+				}
+			}
+
+		}
+
+		// DELETE ==================================================================
+		// delete a todo after checking it
+		$scope.deleteProduto = function(id) {
+			$scope.loading = true;
+
+			//Chama public/js/services/produtos.js
+			Produtos.excluir(id)
+				// if successful creation, call our get function to get all the new produtos
+				.success(function(data) {
+					$scope.loading = false;
+					$scope.produtos = data; // assign our new list of produtos
+				});
+		};
+	}]);
